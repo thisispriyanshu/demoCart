f7d0e07639460a18dcf9f928938cca79ca572cce
thisispriyanshu <priyanshuagrawal801@gmail.com>
2021-09-09 17:34:50 +0530

    quantity and price is changing

--- mycart/src/components/CartList.js
+++ mycart/src/components/CartList.js
@@ -4,7 +4,61 @@ import AddIcon from '@material-ui/icons/Add';
 import RemoveIcon from '@material-ui/icons/Remove';
 import DeleteIcon from '@material-ui/icons/Delete';
 
-function CartList(props) {   
+class CartList extends React.Component { 
+    constructor(props){
+      super(props);
+      this.state={
+          quantity: 1,
+          price : 400,
+          show : true,
+          min: 0,
+      };
+    }
+    IncrementItem=()=>{
+        this.setState(prevState=>{
+            if(prevState.quantity >-1){
+                return{
+                    quantity: prevState.quantity +1,
+                    price : prevState.price + 400,
+                }
+            }else{
+                return null;
+            }
+        })
+    }
+    DecrementItem=()=>{
+        this.setState(prevState=>{
+            if(prevState.quantity >0){
+                return{
+                    quantity: prevState.quantity -1,
+                    price : prevState.price - 400,
+                }
+            }else{
+                return null;
+            }
+        })
+    }
+    DeleteItem=()=>{
+        this.setState(prevState=>{
+            if(prevState.quantity >0){
+                return{
+                    quantity: 0,
+                    price : 0,
+                }
+            }else{
+                return null;
+            }
+        })
+    }
+    ToggleClick = () => {
+        this.setState({
+          show: !this.state.show
+        });
+      }
+      handleChange = (event) => {
+        this.setState({quantity: event.target.value});
+      }
+      render(){
     return (
         <div className="cartlist">
             <div className="cart-item">
@@ -15,13 +69,13 @@ function CartList(props) {
                     Laptop
                 </div>
                 <div className="cart-item-button">
-                   <AddIcon/>
-                   <RemoveIcon/>
-                   <DeleteIcon/>
+                   <AddIcon onClick={this.IncrementItem}/>
+                   <RemoveIcon onClick={this.DecrementItem}/>
+                   <DeleteIcon onClick={this.DeleteItem}/>
                 </div>
                 <div className="cart-item-descr">
-                    <p>Quantity : </p>
-                    <p>Price : </p>
+                    <input value={this.state.quantity} onChange={this.handleChange} ></input>
+                    <input value={this.state.price} onChange={this.handleChange}></input>
                 </div>
             </div>
             <div className="cart-item">
@@ -32,17 +86,18 @@ function CartList(props) {
                     Laptop
                 </div>
                 <div className="cart-item-button">
-                   <AddIcon/>
-                   <RemoveIcon/>
-                   <DeleteIcon/>
+                <AddIcon onClick={this.IncrementItem}/>
+                   <RemoveIcon onClick={this.DecrementItem}/>
+                   <DeleteIcon onClick={this.DeleteItem}/>
                 </div>
                 <div className="cart-item-descr">
-                    <p>Quantity</p>
-                    <p>Price</p>
+                <input value={this.state.quantity} onChange={this.handleChange} ></input>
+                    <input value={this.state.price} onChange={this.handleChange}></input>
                 </div>
             </div>
         </div>
     );
+      }
 }
 
 export default CartList;
