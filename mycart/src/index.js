1dc817f4f28412603e54d2c87a4ff5b295216a72
thisispriyanshu <priyanshuagrawal801@gmail.com>
2021-10-14 00:36:38 +0530

    firebase error-2

--- mycart/src/index.js
+++ mycart/src/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
+// import firebase from 'firebase/firebase';
+// const firebase = require("firebase");
+// Required for side-effects
+// require("firebase/firestore");
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +15,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
